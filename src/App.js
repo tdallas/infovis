e3bb7d4752fc43f81de81bc4220bef0cf18c4dbd
tdallas <tdallas@itba.edu.ar>
2021-03-15 19:35:25 -0300

    feat: added basic styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import { SeatsHeld } from "./pages/seats_held/SeatsHeld";
+import { makeStyles } from "@material-ui/core/styles";
 
-const App = () => (
-  <div>
-    <SeatsHeld />
-  </div>
-);
+const useStyles = makeStyles({
+  app: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: "8px",
+  },
+});
+
+const App = () => {
+  const { app } = useStyles();
+  return (
+    <div className={app}>
+      <SeatsHeld />
+    </div>
+  );
+};
 
 export default App;
--- src/viz/seats_held_by_woman.js
+++ src/viz/seats_held_by_woman.js
@@ -1,16 +1,5 @@
-const csv = require("csv-parser");
-const fs = require("fs");
-
-const results = {};
-
-fs.createReadStream("../data-src/seats_held.csv")
-  .pipe(csv())
-  .on("data", (data) => {
-    const { Location } = data;
-    delete data.Location;
-    results[Location] = data;
-  })
-  .on("end", () => console.log("results", results));
-
-// right know i have results parsed
-
+export const getSeatsParsed = () => {
+  const seatsHeldParsed = {};
+  console.log("seatsHeld", seatsHeld);
+  return seatsHeldParsed;
+};

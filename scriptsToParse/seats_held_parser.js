fcf2c61e342efd57278e0240c92654ab6c122762
tdallas <tdallas@itba.edu.ar>
2021-03-15 20:29:54 -0300

    feat: added new json

--- scriptsToParse/seats_held_parser.js
+++ scriptsToParse/seats_held_parser.js
@@ -1,23 +1,17 @@
 const csv = require("csv-parser");
 const fs = require("fs");
 
-const results = {};
+const results = [];
 
 fs.createReadStream("data-src/seats_held.csv")
   .pipe(csv())
   .on("data", (data) => {
-    const { Location } = data;
-    delete data.Location;
-    results[Location] = data;
+    results.push(data);
   })
   .on("end", () => {
-    fs.writeFile(
-      "seats_held.json",
-      JSON.stringify(results),
-      (error) => {
-        if (error) throw error;
-      }
-    );
+    fs.writeFile("seats_held.json", JSON.stringify(results), (error) => {
+      if (error) throw error;
+    });
   });
 
 // right know i have results parsed
